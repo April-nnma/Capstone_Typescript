e56a1be44b4f5bb01504f6d95f48309f1fda78e9
HaKien <nguyenhakien99@gmail.com>
2023-09-14 21:26:12 +0700

    first commit

---
+++ k_cinema/src/router/index.ts
@@ -0,0 +1,11 @@
+import { RouteObject } from "react-router-dom";
+import { PATH } from "../constant/config";
+
+export const router: RouteObject[] = [
+    {
+        path: PATH.login,
+    },
+    {
+        path: PATH.register,
+    },
+];
