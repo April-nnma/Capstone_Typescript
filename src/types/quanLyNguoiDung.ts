648b285332e2a62abb70a0d9cb359399b829c074
Nguyen Anh <nguyenanh1904.vn@gmail.com>
2023-10-06 23:46:02 +0700

    fix

--- src/types/quanLyNguoiDung.ts
+++ src/types/quanLyNguoiDung.ts
@@ -1,25 +1,16 @@
 export type UserLogin = {
-    taiKhoan: string;
-    hoTen: string;
-    email: string;
-    soDT: string;
-    maNhom: string;
-    maLoaiNguoiDung: "KhachHang" | "QuanTri";
-    accessToken: string;
-};
+    taiKhoan: string
+    hoTen: string
+    email: string
+    soDT: string
+    maNhom: string
+    maLoaiNguoiDung: 'KhachHang' | 'QuanTri'
+    accessToken: string
+}
 
-export type UserByAccessToken = Omit<UserLogin, "accessToken"> & {
+export type UserByAccessToken = Omit<UserLogin, 'accessToken'> & {
+    thongTinDatVe?: []
     loaiNguoiDung: {
-        maLoaiNguoiDung: "KhachHang" | "QuanTri";
-    };
-};
-
-export type Update = {
-    taiKhoan: string;
-    matKhau: string;
-    email: string;
-    soDt: string;
-    maNhom: string;
-    maLoaiNguoiDung: string;
-    hoTen: string;
-};
+        maLoaiNguoiDung: 'KhachHang' | 'QuanTri'
+    }
+}
